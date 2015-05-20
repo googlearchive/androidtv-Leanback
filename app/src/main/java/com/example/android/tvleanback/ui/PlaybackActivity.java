99d18c5b8570f4b5d9735d9b01eed6e5a9ccf61d
shawn shen <shawnshen@google.com>
2015-05-20 13:25:33 -0700

    Fixed memory issue in DetailsFragment and background manager Switch from setDrawables to setBitmap for background manager; call release in onStop; code refactoring Added mVideoView.setVideoPath(null) in onDestroy to avoid activity leak
    Change-Id: I8b352e151f980a3ea556a9cb61ef9d40aaea6199

--- app/src/main/java/com/example/android/tvleanback/ui/PlaybackActivity.java
+++ app/src/main/java/com/example/android/tvleanback/ui/PlaybackActivity.java
@@ -71,6 +71,7 @@ public class PlaybackActivity extends Activity {
         super.onDestroy();
         stopPlayback();
         mVideoView.suspend();
+        mVideoView.setVideoPath(null);
         mSession.release();
     }
 
