ccce347c0b8e0b7f97032308ff3806261a7629bf
Nick Felker <fleker@google.com>
2016-06-20 12:53:29 -0700

    Fixes issue where voice search cannot be reselected after empty search
    Bug: 29506336
    Change-Id: Id735714adcd70e974f3855d381f1b81be7444a87

--- app/src/main/java/com/example/android/tvleanback/ui/SearchFragment.java
+++ app/src/main/java/com/example/android/tvleanback/ui/SearchFragment.java
@@ -71,6 +71,7 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
             new CursorObjectAdapter(new CardPresenter());
 
     private int mSearchLoaderId = 1;
+    private boolean mResultsFound = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -150,7 +151,7 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
     }
 
     public boolean hasResults() {
-        return mRowsAdapter.size() > 0;
+        return mRowsAdapter.size() > 0 && mResultsFound;
     }
 
     private boolean hasPermission(final String permission) {
@@ -164,6 +165,10 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
             mQuery = query;
             getLoaderManager().initLoader(mSearchLoaderId++, null, this);
         }
+    }
+
+    public void focusOnSearch() {
+        getView().findViewById(R.id.lb_search_bar).requestFocus();
     }
 
     @Override
@@ -182,20 +187,19 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        int titleRes;
         if (cursor != null && cursor.moveToFirst()) {
-            mVideoCursorAdapter.changeCursor(cursor);
-
-            mRowsAdapter.clear();
-            HeaderItem header = new HeaderItem(getString(R.string.search_results, mQuery));
-            ListRow row = new ListRow(header, mVideoCursorAdapter);
-            mRowsAdapter.add(row);
+            mResultsFound = true;
+            titleRes = R.string.search_results;
         } else {
-            // No results were found.
-            mRowsAdapter.clear();
-            HeaderItem header = new HeaderItem(getString(R.string.no_search_results, mQuery));
-            ListRow row = new ListRow(header, new ArrayObjectAdapter());
-            mRowsAdapter.add(row);
+            mResultsFound = false;
+            titleRes = R.string.no_search_results;
         }
+        mVideoCursorAdapter.changeCursor(cursor);
+        HeaderItem header = new HeaderItem(getString(titleRes, mQuery));
+        mRowsAdapter.clear();
+        ListRow row = new ListRow(header, mVideoCursorAdapter);
+        mRowsAdapter.add(row);
     }
 
     @Override
